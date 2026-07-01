ece1c74c7a48d90ba040b36772233249224ab685
Maru <238571780+marumakes@users.noreply.github.com>
2026-07-01 18:46:58 +0900

    chore: initial commit

---
+++ boards/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.conf import settings
+
+class Board(models.Model):
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add = True)
+
+class List(models.Model):
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='list')
+    name = models.CharField(max_length=100)
+    position = models.PositiveIntegerField(default = 0)
+
+class Card(models.Model):
+    list = models.ForeignKey(List,on_delete=models.CASCADE, related_name="cards")
+    title = models.CharField(max_length = 200)
+    description = models.TextField(blank = True)
+    position = models.PositiveIntegerField(default = 0)
